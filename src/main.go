9d24630ac23dadf0969a3500caa985520d9fd675
Andre de Andrade <vgip88@gmail.com>
2026-05-28 12:02:42 -0300

    mais de 1 upgrade

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo versao 3.0 super super super")
+	fmt.Fprintf(w, "Aplicacao exemplo versao 3.0 super mais de um upgrade")
 }
 
 func main() {
